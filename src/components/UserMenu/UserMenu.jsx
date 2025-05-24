8cde351f63fa0ae4ef42818a32c5ab52c1d9d597
karyna <karishka.spark@gmail.com>
2025-05-24 05:29:46 +0300

    fixed

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import s from "../Navigation/Navigation.module.css";
 import { selectUser } from "../../redux/auth/selectors";
+import { logoutThunk } from "../../redux/auth/operations";
 
 const UserMenu = () => {
   const dispatch = useDispatch();
@@ -9,7 +10,7 @@ const UserMenu = () => {
   return (
     <div className={s.userMenu}>
       <span className={s.userName}>{user.name}</span>
-      <button className={s.btn} onClick={() => dispatch(logoutThun())}>
+      <button className={s.btn} onClick={() => dispatch(logoutThunk())}>
         Logout
       </button>
     </div>
